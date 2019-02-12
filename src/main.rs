a201009d1132d17573550504b4c7dae738b8cab5
Jilles Tjoelker <jilles@stack.nl>
2019-02-13 00:20:47 +0100

    Add form to schedule/pair a round

--- src/main.rs
+++ src/main.rs
@@ -80,11 +80,56 @@ fn index(state: State<AppState>) -> impl Responder {
     IndexTemplate { games }
 }
 
+#[derive(Debug)]
+struct Round {
+    id: i32,
+    date: String,
+}
+
+#[derive(Debug)]
+struct Presence {
+    player_id: i32,
+    name: String,
+    schedule: bool,
+}
+
+#[derive(Template)]
+#[template(path = "schedule_round.html")]
+struct ScheduleRoundTemplate {
+    round: Round,
+    presences: Vec<Presence>,
+}
+
+fn schedule_round((params, state): (Path<(i32,)>, State<AppState>)) -> impl Responder {
+    let round_id = params.0;
+    let conn = state.dbpool.get().unwrap();
+    let rows = conn.query("SELECT date::TEXT FROM rounds WHERE id=$1;", &[&round_id]).unwrap();
+    let round_date = rows.iter().next().map_or_else(|| "??".to_owned(), |row| {
+        let date: String = row.get(0);
+        date
+    });
+    let round = Round { id: round_id, date: round_date };
+    let rows = conn.query("SELECT pl.id, pl.name, pr.schedule FROM players pl, presence pr WHERE pl.id = pr.player AND \"when\"=$1;",
+        &[&round_id]).unwrap();
+    let presences: Vec<Presence> = rows.iter().map(|row| {
+        let player_id: i32 = row.get(0);
+        let name: String = row.get(1);
+        let schedule: bool = row.get(2);
+        Presence {
+            player_id,
+            name,
+            schedule
+        }
+    }).collect();
+    ScheduleRoundTemplate { round, presences }
+}
+
 fn main() {
     let dbpool = Arc::new(db::create_pool());
     server::new(
         move || App::with_state(AppState { dbpool: dbpool.clone() })
-            .route("/index.html", http::Method::GET, index))
+            .route("/", http::Method::GET, index)
+            .resource("/schedule/{round}", |r| r.method(http::Method::GET).with(schedule_round)))
         .bind("127.0.0.1:8080").unwrap()
         .run();
 }
