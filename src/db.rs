e9a6e4e2dd6cc9a239f78360c425ef909966a5f8
Jilles Tjoelker <jilles@stack.nl>
2019-03-04 22:26:30 +0100

    Fix formatting

--- src/db.rs
+++ src/db.rs
@@ -4,8 +4,6 @@ pub type Pool = r2d2::Pool<r2d2_postgres::PostgresConnectionManager>;
 pub type Connection = r2d2::PooledConnection<r2d2_postgres::PostgresConnectionManager>;
 
 pub fn create_pool(url: &str) -> Pool {
-    let manager =
-        PostgresConnectionManager::new(url, TlsMode::None)
-            .unwrap();
+    let manager = PostgresConnectionManager::new(url, TlsMode::None).unwrap();
     Pool::builder().max_size(2).build(manager).unwrap()
 }
